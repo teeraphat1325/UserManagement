bfcdea54326f0e0e6c2867c78e270ef2ec7e3899
teeraphat1325 <teeraphat1325@gmail.com>
2024-08-18 10:50:32 +0700

    Innitial Project

---
+++ src/main/java/com/mycompany/usermanagement/UserManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usermanagement;
+
+/**
+ *
+ * @author teeraphat
+ */
+public class UserManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
