a01c60f02d453d2fc1b217ef190aa65d79ebc222
teeraphat1325 <teeraphat1325@gmail.com>
2024-08-18 20:59:16 +0700

    UserService finish

---
+++ src/main/java/com/mycompany/usermanagement/UserService.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.usermanagement;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author teeraphat
+ */
+public class UserService{
+    private static ArrayList<User> userList = new ArrayList<>();
+    private static int lastid = 1;
+    
+    public static User addUser(User newUser){
+        newUser.setId(lastid++);
+        userList.add(newUser);
+        return newUser;
+        
+    }
+    
+    public static ArrayList<User> getUsers(){
+        return userList;
+    }
+    
+    public static User getUser(int index){
+        return userList.get(index);
+    }
+    
+    public static User getUserById(int id){
+        for(User u:userList){
+            if(u.getId()==id){
+                return u;
+            }
+        }
+        return null;
+    }
+    
+    public static int getSize(){
+        return userList.size();
+    }
+    
+    public static User updateUser(int index,User user){
+        userList.set(index,user);
+        return user;
+    }
+    
+    public static User deleteUser(int index){
+        return userList.remove(index);
+    }
+    
+    public static void printList(){
+        ArrayList<User> list = UserService.getUsers();
+        for(User u: list){
+            System.out.println(u);
+        }
+    }
+    
+}
--- src/main/java/com/mycompany/usermanagement/testUser.java
+++ src/main/java/com/mycompany/usermanagement/testUser.java
@@ -13,14 +13,23 @@ import java.util.ArrayList;
 public class testUser {
     public static void main(String[] args) {
         User admin = new User(1, "admin", "Administrator", "pass01234", 'M', 'A');
-        User user1 = new User(1, "user1", "User 1", "pass01234", 'F', 'U');
-        User user2 = new User(1, "user2", "User2", "pass01234", 'M', 'U');
-        ArrayList userList = new ArrayList<User>();
+        User user1 = new User(2, "user1", "User 1", "pass01234", 'F', 'U');
+        User user2 = new User(3, "user2", "User2", "pass01234", 'M', 'U');
+        ArrayList<User> userList = new ArrayList<>();
         userList.add(admin);
         userList.add(user1);
         userList.add(user2);
         for(int i=0;i<userList.size();i++){
             System.out.println(userList.get(i));
+            
         }
+        
+        UserService.addUser(admin);
+        UserService.addUser(user1);
+        UserService.addUser(user2);
+        System.out.println("*************************************");
+        UserService.printList();
+        
+        System.out.println(UserService.getUserById(3));
     }
 }
